0d41be10f6ee8e41668e99e75566f501350bc7b7
Amparo Guillén Requena <ampguireq@gmail.com>
2023-09-27 18:46:56 +0100

    Final Sección 12: Pipes Personalizados

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,16 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { OrderComponent } from './pages/order/order.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { ProductsRoutingModule } from './products-routing.module';
-import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+
+import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+
 
 
 @NgModule({
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    OrderComponent,
+    UncommonPageComponent,
+
+    //Pipes
+    CanFlyPipe,
+    SortByPipe,
+    ToggleCasePipe,
   ],
   imports: [
     CommonModule,
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -41,8 +41,9 @@ export class MenuComponent implements OnInit {
         icon: 'pi pi-cog',
         items: [
           {
-            label: 'Otro elemento',
+            label: 'Custon Pipes',
             icon: 'pi pi-cog',
+            routerLink: 'custom'
           }
         ]
       }
